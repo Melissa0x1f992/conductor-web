b3483df5a9956a82d6a1dac785b85332c72c373f
Isacc Davis <Melissa0x1f992@gmail.com>
2021-10-09 14:57:31 -0500

    Add Hello World example from ExpressJS Docs

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-'use strict';
+'use strict'
+
+import express from "express"
+
+const port = 80
+
+const server = express()
+
+server.get('/', (req, res) => {
+	res.send('hi helo')
+})
+
+server.listen(port, () => {
+	console.log(`Ready to say helo at http://localhost:${port}`)
+})
