9c548e0215d1829afe9da795cb151f4a516fed20
Isacc Davis <Melissa0x1f992@gmail.com>
2021-10-10 17:02:00 -0500

    Add form for color set

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-'use strict';
+'use strict'
+
+import express from 'express'
+
+const port = 8000
+const host = '192.168.1.10'
+
+const app = express()
+
+app.get('/*', (req, res) => {
+	console.log(`${req.hostname}${req.originalUrl}`)
+
+	res.status(302)
+	res.send(
+		"<html>" +
+		"<head>" +
+		"	<title>Isacc's Lights</title>" +
+		"</head>" +
+		"<body>" +
+		"	<form>" +
+		"		<input />" +
+		"		<input />" +
+		"	</form>" +
+		"</body>" +
+		"</html>"
+	)
+})
+
+app.listen(port, host, () => {
+	console.log(`Listening on http://${host}:${port}`)
+})
